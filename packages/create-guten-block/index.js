8604ae7d685e55a2ac302e2fbd66cabeb2da68e0
Ahmad Awais <mrahmadawais@gmail.com>
2018-01-13 23:22:24 +0500

    📖 DOC: Improvements 💯

--- packages/create-guten-block/index.js
+++ packages/create-guten-block/index.js
@@ -1,31 +1,29 @@
 #!/usr/bin/env node
-
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
+ * Main create-guten-block app
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * Check the node version if above 8 then run the app.
  */
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //   /!\ DO NOT MODIFY THIS FILE /!\
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// create-react-app is installed globally on people's computers. This means
+// create-guten-block is installed globally on people's computers. This means
 // that it is extremely difficult to have them upgrade the version and
 // because there's only one global version installed, it is very prone to
 // breaking changes.
 //
-// The only job of create-react-app is to init the repository and then
-// forward all the commands to the local version of create-react-app.
+// The only job of create-guten-block is to init the repository and then
+// forward all the commands to the local version of create-guten-block.
 //
 // If you need to add a new command, please add it to the scripts/ folder.
 //
 // The only reason to modify this file is to add more warnings and
-// troubleshooting information for the `create-react-app` command.
+// troubleshooting information for the `create-guten-block` command.
 //
 // Do not make breaking changes! We absolutely don't want to have to
-// tell people to update their global version of create-react-app.
+// tell people to update their global version of create-guten-block.
 //
 // Also be careful with new language features.
 // This file must work on Node 0.10+.
@@ -37,12 +35,12 @@
 'use strict';
 
 const chalk = require( 'chalk' );
-
 const currentNodeVersion = process.versions.node;
 const semver = currentNodeVersion.split( '.' );
 const major = semver[ 0 ];
 
-if ( major < 4 ) {
+// If below Node 8.
+if ( major < 8 ) {
 	console.error(
 		chalk.red(
 			'You are running Node ' +
@@ -55,4 +53,5 @@ if ( major < 4 ) {
 	process.exit( 1 );
 }
 
+// Otherwise, run the app.
 require( './createGutenBlock' );
--- tasks/cgb.js
+++ tasks/cgb.js
@@ -3,7 +3,8 @@
  * CGB Command Run for Dev ENV.
  */
 
-// Run CGB command in development env.
+// Runs CGB command in development env.
+// Runs lerna bootstrap.
 // Then handleErrors.
 // TODO: Allow custom scripts.
 
@@ -35,35 +36,32 @@ const handleError = e => {
 process.on( 'SIGINT', handleExit );
 process.on( 'uncaughtException', handleError );
 
-// The rootDir is where this repo exits. i.e. the dir where lerna.json is.
-const rootDir = path.join( __dirname, '..' );
-
 // ******************************************************************************
-// TODO: Allow custom scripts.
 // Pack cgb-scripts so we can verify they work.
 // ******************************************************************************
 
-// // The rootDir is where this repo exits. i.e. the dir where lerna.json is.
-// const rootDir = path.join( __dirname, '..' );
-// const cgbScriptsDir = path.join( rootDir, 'packages', 'cgb-scripts' );
-// const packageJsonPath = path.join( cgbScriptsDir, 'package.json' );
-// const packageJsonOrigPath = path.join( cgbScriptsDir, 'package.json.orig' );
+// The rootDir is where this repo exits. i.e. the dir where lerna.json is.
+const rootDir = path.join( __dirname, '..' );
+const cgbScriptsDir = path.join( rootDir, 'packages', 'cgb-scripts' );
+const packageJsonPath = path.join( cgbScriptsDir, 'package.json' );
+const packageJsonOrigPath = path.join( cgbScriptsDir, 'package.json.orig' );
 
-// // Install all our packages
-// const lernaPath = path.join( rootDir, 'node_modules', '.bin', 'lerna' );
-// cp.execSync( `${ lernaPath } bootstrap`, {
-// 	cwd: rootDir,
-// 	stdio: 'inherit',
-// } );
+// Install all our packages.
+const lernaPath = path.join( rootDir, 'node_modules', '.bin', 'lerna' );
+cp.execSync( `${ lernaPath } bootstrap`, {
+	cwd: rootDir,
+	stdio: 'inherit',
+} );
 
-// // Save package.json because we're going to touch it
-// fs.writeFileSync( packageJsonOrigPath, fs.readFileSync( packageJsonPath ) );
+// Save package.json because we're going to touch it
+fs.writeFileSync( packageJsonOrigPath, fs.readFileSync( packageJsonPath ) );
 
-// // Replace own dependencies (those in the`packages` dir) with the local paths
-// // of those packages
-// const replaceOwnDepsPath = path.join( __dirname, 'replace-own-deps.js' );
-// cp.execSync( `node ${ replaceOwnDepsPath }`, { stdio: 'inherit' } );
+// Replace own dependencies (those in the`packages` dir) with the local paths
+// of those packages
+const replaceOwnDepsPath = path.join( __dirname, 'replace-own-deps.js' );
+cp.execSync( `node ${ replaceOwnDepsPath }`, { stdio: 'inherit' } );
 
+// TODO: Allow custom scripts.
 // // Finally, pack cgb-scripts
 // // Don't redirect stdio as we want to capture the output that will be returned
 // // from execSync(). In this case it will be the .tgz filename.
@@ -79,10 +77,10 @@ const rootDir = path.join( __dirname, '..' );
 // 	scriptsFileName
 // );
 
-// // Restore package.json
-// fs.unlinkSync( packageJsonPath );
-// fs.writeFileSync( packageJsonPath, fs.readFileSync( packageJsonOrigPath ) );
-// fs.unlinkSync( packageJsonOrigPath );
+// Restore package.json
+fs.unlinkSync( packageJsonPath );
+fs.writeFileSync( packageJsonPath, fs.readFileSync( packageJsonOrigPath ) );
+fs.unlinkSync( packageJsonOrigPath );
 
 // ******************************************************************************
 // Now that we have packed them, call the global CLI.
