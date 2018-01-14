2a302d60ce9dbafd7ea1cdcd6f89454b5537a9b5
Ahmad Awais <mrahmadawais@gmail.com>
2018-01-14 21:35:03 +0500

    📦 NEW: Eject Script

--- packages/cgb-scripts/config/paths.js
+++ packages/cgb-scripts/config/paths.js
@@ -11,8 +11,26 @@ const fs = require( 'fs' );
 const pluginDir = fs.realpathSync( process.cwd() );
 const resolveApp = relativePath => path.resolve( pluginDir, relativePath );
 
+// Config after eject: we're in ./config/
+module.exports = {
+	dotenv: resolveApp( '.env' ),
+	pluginBlocksJs: resolveApp( 'src/blocks.js' ),
+	yarnLockFile: resolveApp( 'yarn.lock' ),
+	pluginDist: resolveApp( '.' ), // We are in ./dist folder already so the path '.' resolves to ./dist/.
+};
+
+// @remove-on-eject-begin
+const resolveOwn = relativePath => path.resolve( __dirname, '..', relativePath );
+
+// config before eject: we're in ./node_modules/react-scripts/config/
 module.exports = {
 	dotenv: resolveApp( '.env' ),
 	pluginBlocksJs: resolveApp( 'src/blocks.js' ),
 	pluginDist: resolveApp( '.' ), // We are in ./dist folder already so the path '.' resolves to ./dist/.
+	yarnLockFile: resolveApp( 'yarn.lock' ),
+	appPath: resolveApp( '.' ),
+	// These properties only exist before ejecting:
+	ownPath: resolveOwn( '.' ),
 };
+
+console.log( resolveApp( '.' ) );
