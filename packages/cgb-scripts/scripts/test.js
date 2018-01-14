87335056f80cde9023680881a931c36eb25febcf
Ahmad Awais <mrahmadawais@gmail.com>
2018-01-14 20:25:31 +0500

    📦 NEW: Build Script

--- packages/cgb-scripts/scripts/test.js
+++ packages/cgb-scripts/scripts/test.js
@@ -1,6 +1,2 @@
+console.clear();
 console.log( 'MANUAL TEST!' );
-
-const resolvePkg = require( 'resolve-pkg' );
-const cgbDevUtils = resolvePkg( 'cgb-dev-utils', { cwd: __dirname } );
-
-console.log( cgbDevUtils );
