abcd7e32e901bc55a8ae02cbc7d4226cba3d9a01
Maedah Batool <MaedahBatool@gmail.com>
2018-01-16 20:47:34 +0500

    📖 DOC: First iteration

--- packages/create-guten-block/createGutenBlock.js
+++ packages/create-guten-block/createGutenBlock.js
@@ -8,6 +8,13 @@ const execa = require( 'execa' );
 const shell = require( 'shelljs' );
 const directoryExists = require( 'directory-exists' );
 
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on( 'unhandledRejection', err => {
+	throw err;
+} );
+
 /**
  * Cross platform clear console.
  */
