0dd5fb33745146242131dbad193927cb2712b952
Ahmad Awais <mrahmadawais@gmail.com>
2018-01-22 12:47:45 +0500

    📦 NEW: Example Multi-block

---
+++ examples/02-multi-block/src/init.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Blocks Initializer
+ *
+ * Enqueue CSS/JS of all the blocks.
+ *
+ * @since 	1.0.0
+ * @package CGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Enqueue Gutenberg block assets for both frontend + backend.
+ *
+ * `wp-blocks`: includes block type registration and related functions.
+ *
+ * @since 1.0.0
+ */
+function multi_block_cgb_block_assets() {
+	// Styles.
+	wp_enqueue_style(
+		'multi_block-cgb-style-css', // Handle.
+		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
+		array( 'wp-blocks' ) // Dependency to include the CSS after it.
+		// filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' ) // Version: filemtime — Gets file modification time.
+	);
+} // End function multi_block_cgb_block_assets().
+
+// Hook: Frontend assets.
+add_action( 'enqueue_block_assets', 'multi_block_cgb_block_assets' );
+
+/**
+ * Enqueue Gutenberg block assets for backend editor.
+ *
+ * `wp-blocks`: includes block type registration and related functions.
+ * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
+ * `wp-i18n`: To internationalize the block's text.
+ *
+ * @since 1.0.0
+ */
+function multi_block_cgb_editor_assets() {
+	// Scripts.
+	wp_enqueue_script(
+		'multi_block-cgb-block-js', // Handle.
+		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element' ) // Dependencies, defined above.
+		// filemtime( plugin_dir_path( __FILE__ ) . 'block.js' ) // Version: filemtime — Gets file modification time.
+	);
+
+	// Styles.
+	wp_enqueue_style(
+		'multi_block-cgb-block-editor-css', // Handle.
+		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
+		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
+		// filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' ) // Version: filemtime — Gets file modification time.
+	);
+} // End function multi_block_cgb_editor_assets().
+
+// Hook: Editor assets.
+add_action( 'enqueue_block_editor_assets', 'multi_block_cgb_editor_assets' );
