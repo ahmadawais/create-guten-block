08aba62c2a4b5416821537bfa672871cf06c0521
Ahmad Awais <mrahmadawais@gmail.com>
2018-01-22 12:32:17 +0500

    👌 IMPROVE: Todo & Fromat

--- packages/cgb-scripts/scripts/block.js
+++ packages/cgb-scripts/scripts/block.js
@@ -1,7 +1,9 @@
 /**
  * New Block plugin intializer
  *
+ * TODO
  * - Work in progress
+ * - First pass
  *
  * - Copies the templates.
  * - Prints final instructions.
@@ -86,60 +88,10 @@ const copyTemplateFiles = (
  * @param {string} blockName The block name.
  * @param  {string} blockDir The block directory.
  */
-const printNextSteps = ( blockName, blockDir ) => {
+const printNextSteps = blockName => {
 	console.log(
 		'\n\n✅ ',
-		chalk.black.bgGreen( ' All done! Go build some Gutenberg blocks. \n' )
-	);
-	console.log(
-		`CGB ${ chalk.dim(
-			'(create-guten-block)'
-		) } has created a WordPress plugin called `,
-		chalk.dim( blockName ),
-		' that you can use with zero configurations #0CJS to build Gutenberg blocks with ESNext (i.e. ES6/7/8), React.js, JSX, Webpack, ESLint, etc.'
-	);
-	console.log(
-		`\nCreated ${ chalk.dim( blockName ) } plugin at: ${ chalk.dim( blockDir ) }`,
-		'\nInside that directory, you can run several commands:\n'
-	);
-
-	console.log(
-		'\n👉 ',
-		' Type',
-		chalk.black.bgWhite( ' npm start ' ),
-		'\n\n',
-		'     Use to compile and run the block in development mode.',
-		'\n',
-		'     Watches for any changes and reports back any errors in your code.'
-	);
-	console.log(
-		'\n\n👉 ',
-		' Type',
-		chalk.black.bgWhite( ' npm run build ' ),
-		'\n\n',
-		'     Use to build production code for your block inside dist folder.',
-		'\n',
-		'     Runs once and reports back the gzip file sizes of the produced code.'
-	);
-
-	console.log(
-		'\n👉 ',
-		' Type',
-		chalk.black.bgWhite( ' npm run eject ' ),
-		'\n\n',
-		'     Removes this tool and copies build dependencies, configuration files',
-		'\n',
-		'     and scripts into the plugin folder. ⚠️  It\'s a one way street.',
-		'\n',
-		'     If you do this, you can’t go back!'
-	);
-
-	console.log( '\n\n', chalk.black.bgGreen( ' Get Started → \n' ) );
-	console.log( ' We suggest that you begin by typing: \n' );
-	console.log(
-		`  ${ chalk.green( 'cd' ) } ${ blockName }`,
-		'\n',
-		` ${ chalk.green( 'npm' ) } start`,
+		chalk.black.bgGreen( ` Created a new block at .src/${ blockName }. ` ),
 		'\n\n'
 	);
 };
@@ -154,6 +106,8 @@ const printNextSteps = ( blockName, blockDir ) => {
  * @param {string} blockName The block name.
  * @param  {string} blockDir The block directory.
  */
+// Define blockName
+// Define blockDir
 module.exports = async( root, blockName, blockDir ) => {
 	// 0. Create block name for PHP functions.
 	const blockNamePHPLower = blockName
@@ -177,5 +131,5 @@ module.exports = async( root, blockName, blockDir ) => {
 	spinner.succeed();
 
 	// 2. Prints next steps.
-	await printNextSteps( blockName, blockDir );
+	await printNextSteps( blockName );
 };
