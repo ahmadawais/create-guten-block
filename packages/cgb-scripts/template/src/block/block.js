1254c29de9d734d5cbe77073d46e96654622d9e5
Ahmad Awais <me@AhmadAwais.com>
2018-02-03 16:21:04 +0500

    📖 DOC: Better inline docs updates

--- packages/cgb-scripts/template/src/block/block.js
+++ packages/cgb-scripts/template/src/block/block.js
@@ -19,6 +19,7 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  * behavior. Once registered, the block is made editor as an option to any
  * editor interface where blocks are implemented.
  *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
  * @param  {string}   name     Block name.
  * @param  {Object}   settings Block settings.
  * @return {?WPBlock}          The block, if it has been successfully
@@ -26,7 +27,7 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  */
 registerBlockType( 'cgb/block-<% blockName %>', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( '<% blockName %> - CGB Block', 'CGB' ), // Block title.
+	title: __( '<% blockName %> - CGB Block' ), // Block title.
 	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
@@ -35,7 +36,14 @@ registerBlockType( 'cgb/block-<% blockName %>', {
 		__( 'create-guten-block' ),
 	],
 
-	// The "edit" property must be a valid function.
+	/**
+	 * The edit function describes the structure of your block in the context of the editor.
+	 * This represents what the editor will render when the block is used.
+	 *
+	 * The "edit" property must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
 	edit: function( props ) {
 		// Creates a <p class='wp-block-cgb-block-<% blockName %>'></p>.
 		return (
@@ -56,7 +64,14 @@ registerBlockType( 'cgb/block-<% blockName %>', {
 		);
 	},
 
-	// The "save" property must be specified and must be a valid function.
+	/**
+	 * The save function defines the way in which the different attributes should be combined
+	 * into the final markup, which is then serialized by Gutenberg into post_content.
+	 *
+	 * The "save" property must be specified and must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
 	save: function( props ) {
 		return (
 			<div className={ props.className }>
