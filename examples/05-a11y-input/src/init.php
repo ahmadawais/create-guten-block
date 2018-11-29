60cc4298b013c2256d0a2546b33a22bd333ba97c
Ahmad Awais <me@AhmadAwais.com>
2018-11-29 20:52:44 +0500

    👌 IMPROVE: Docs & Standards

--- examples/05-a11y-input/src/init.php
+++ examples/05-a11y-input/src/init.php
@@ -16,8 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Enqueue Gutenberg block assets for both frontend + backend.
  *
- * `wp-editor`: WP editor styles.
- *
+ * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
 function a11y_input_cgb_block_assets() {
@@ -26,7 +25,7 @@ function a11y_input_cgb_block_assets() {
 		'a11y_input-cgb-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
 		array( 'wp-editor' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
+		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
 	);
 } // End function a11y_input_cgb_block_assets().
 
@@ -36,11 +35,10 @@ add_action( 'enqueue_block_assets', 'a11y_input_cgb_block_assets' );
 /**
  * Enqueue Gutenberg block assets for backend editor.
  *
- * `wp-blocks`:  includes block type registration and related functions.
- * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
- * `wp-i18n`:    internationalize the block's text.
- * `wp-editor`:  WP editor styles.
- *
+ * @uses {wp-blocks} for block type registration & related functions.
+ * @uses {wp-element} for WP Element abstraction — structure of blocks.
+ * @uses {wp-i18n} to internationalize the block's text.
+ * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
 function a11y_input_cgb_editor_assets() {
@@ -49,7 +47,7 @@ function a11y_input_cgb_editor_assets() {
 		'a11y_input-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: File modification time.
 		true // Enqueue the script in the footer.
 	);
 
@@ -58,7 +56,7 @@ function a11y_input_cgb_editor_assets() {
 		'a11y_input-cgb-block-editor-css', // Handle.
 		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
 		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
+		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
 	);
 } // End function a11y_input_cgb_editor_assets().
 
