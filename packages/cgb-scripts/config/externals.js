7f1a247ebed51a29895a791f27676de3261aad15
Ahmad Awais <me@AhmadAwais.com>
2018-11-29 21:32:48 +0500

    📦 NEW: Externals for WP + DRY

---
+++ packages/cgb-scripts/config/externals.js
@@ -0,0 +1,46 @@
+/**
+ * Utility methods for use when generating build configuration objects.
+ */
+const { join } = require( 'path' );
+
+/**
+ * Given a string, returns a new string with dash separators converted to
+ * camel-case equivalent. This is not as aggressive as `_.camelCase`, which
+ * which would also upper-case letters following numbers.
+ *
+ * @param {string} string Input dash-delimited string.
+ *
+ * @return {string} Camel-cased string.
+ */
+const camelCaseDash = string => string.replace( /-([a-z])/g, ( match, letter ) => letter.toUpperCase() );
+
+/**
+ * Define externals to load components through the wp global.
+ */
+const externals = [
+	'components',
+	'edit-post',
+	'element',
+	'plugins',
+	'editor',
+	'blocks',
+	'utils',
+	'date',
+	'data',
+	'i18n',
+].reduce(
+	( externals, name ) => ( {
+		...externals,
+		[ `@wordpress/${ name }` ]: `wp.${ camelCaseDash( name ) }`,
+	} ),
+	{
+		wp: 'wp',
+		ga: 'ga', // Old Google Analytics.
+		gtag: 'gtag', // New Google Analytics.
+		react: 'React', // React itself is there in Gutenberg.
+		jquery: 'jQuery', // import $ from 'jquery' // Use the WordPress version after enqueuing it.
+		'react-dom': 'ReactDOM',
+	}
+);
+
+module.exports = externals;
