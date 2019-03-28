bc4ca8249bd6db117a49e5be788f0e8dfbabd637
Ashar Irfan <mrasharirfan@gmail.com>
2019-03-28 18:48:51 +0500

    🐛 FIX: wp-editor Dependency

--- packages/cgb-scripts/template/src/init.php
+++ packages/cgb-scripts/template/src/init.php
@@ -35,7 +35,7 @@ function <% blockNamePHPLower %>_cgb_block_assets() { // phpcs:ignore
 	wp_register_script(
 		'<% blockNamePHPLower %>-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ), // Dependencies, defined above.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
 		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
