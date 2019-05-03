f558514efaa90aca5fb0bb6c82903f33304a9a73
Ashar Irfan <mrasharirfan@gmail.com>
2019-05-04 03:28:36 +0500

    👌 IMPROVE: uglifyjs-webpack-plugin options for production config

--- packages/cgb-scripts/config/webpack.config.prod.js
+++ packages/cgb-scripts/config/webpack.config.prod.js
@@ -116,7 +116,7 @@ module.exports = {
 			new UglifyJsPlugin( {
 				uglifyOptions: {
 					compress: {
-						warnings: false,
+						// warnings: false,
 						// Disabled because of an issue with Uglify breaking seemingly valid code:
 						// https://github.com/facebookincubator/create-react-app/issues/2376
 						// Pending further investigation:
