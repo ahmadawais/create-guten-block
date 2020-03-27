d52a984366d5b6cd8be7010a80e6d1608299e196
Ashar Irfan <mrasharirfan@gmail.com>
2020-03-28 03:04:33 +0500

    👌 IMPROVE: Dev cgb-scripts config

--- packages/cgb-scripts/config/webpack.config.dev.js
+++ packages/cgb-scripts/config/webpack.config.dev.js
@@ -19,115 +19,13 @@
  *
  * @since 1.0.0
  */
-
-// const autoprefixer = require( 'autoprefixer' );
-// const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-
-// // Extract style.css for both editor and frontend styles.
-// const blocksCSSPlugin = new ExtractTextPlugin( {
-// 	filename: './dist/blocks.style.build.css',
-// } );
-
-// // Extract editor.css for editor styles.
-// const editBlocksCSSPlugin = new ExtractTextPlugin( {
-// 	filename: './dist/blocks.editor.build.css',
-// } );
-
-// // Configuration for the ExtractTextPlugin — DRY rule.
-// const extractConfig = {
-// 	use: [
-// 		// "postcss" loader applies autoprefixer to our CSS.
-// 		{ loader: 'raw-loader' },
-// 		{
-// 			loader: 'postcss-loader',
-// 			options: {
-// 				ident: 'postcss',
-// 				plugins: [
-// 					autoprefixer( {
-// 						browsers: [
-// 							'>1%',
-// 							'last 4 versions',
-// 							'Firefox ESR',
-// 							'not ie < 9', // React doesn't support IE8 anyway
-// 						],
-// 						flexbox: 'no-2009',
-// 					} ),
-// 				],
-// 			},
-// 		},
-// 		// "sass" loader converts SCSS to CSS.
-// 		{
-// 			loader: 'sass-loader',
-// 			options: {
-// 				// Add common CSS file for variables and mixins.
-// 				data: '@import "./src/common.scss";\n',
-// 				outputStyle: 'nested',
-// 			},
-// 		},
-// 	],
-// };
-
-// // Export configuration.
-// module.exports = {
-// 	entry: {
-// 		'./dist/blocks.build': paths.pluginBlocksJs, // 'name' : 'path/file.ext'.
-// 	},
-// 	output: {
-// 		// Add /* filename */ comments to generated require()s in the output.
-// 		pathinfo: true,
-// 		// The dist folder.
-// 		path: paths.pluginDist,
-// 		filename: '[name].js', // [name] = './dist/blocks.build' as defined above.
-// 	},
-// 	// You may want 'eval' instead if you prefer to see the compiled output in DevTools.
-// 	devtool: 'cheap-eval-source-map',
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.(js|jsx|mjs)$/,
-// 				exclude: /(node_modules|bower_components)/,
-// 				use: {
-// 					loader: 'babel-loader',
-// 					options: {
-// 						// @remove-on-eject-begin
-// 						babelrc: false,
-// 						presets: [ require.resolve( 'babel-preset-cgb' ) ],
-// 						// @remove-on-eject-end
-// 						// This is a feature of `babel-loader` for webpack (not Babel itself).
-// 						// It enables caching results in ./node_modules/.cache/babel-loader/
-// 						// directory for faster rebuilds.
-// 						cacheDirectory: true,
-// 					},
-// 				},
-// 			},
-// 			{
-// 				test: /style\.s?css$/,
-// 				exclude: /(node_modules|bower_components)/,
-// 				use: blocksCSSPlugin.extract( extractConfig ),
-// 			},
-// 			{
-// 				test: /editor\.s?css$/,
-// 				exclude: /(node_modules|bower_components)/,
-// 				use: editBlocksCSSPlugin.extract( extractConfig ),
-// 			},
-// 		],
-// 	},
-// 	// Add plugins.
-// 	plugins: [ blocksCSSPlugin, editBlocksCSSPlugin ],
-// 	stats: 'minimal',
-// 	// stats: 'errors-only',
-// 	// Add externals.
-// 	externals: externals,
-// };
-
-const path = require( 'path' );
 const paths = require( './paths' );
 const externals = require( './externals' );
 const postcssPresetEnv = require( 'postcss-preset-env' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const { mode, resolve } = defaultConfig;
+const { mode, resolve, plugins, stats } = defaultConfig;
 
 module.exports = {
 	mode,
@@ -165,10 +63,10 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				use: [
-					path.resolve( MiniCssExtractPlugin.loader ),
-					path.resolve( 'css-loader' ),
+					MiniCssExtractPlugin.loader,
+					'css-loader',
 					{
-						loader: path.resolve( 'postcss-loader' ),
+						loader: 'postcss-loader',
 						options: {
 							ident: 'postcss',
 							plugins: () => [
@@ -185,7 +83,7 @@ module.exports = {
 						},
 					},
 					{
-						loader: path.resolve( 'sass-loader' ),
+						loader: 'sass-loader',
 						options: {
 							prependData: '@import "./src/common.scss";',
 							sassOptions: {
@@ -208,8 +106,11 @@ module.exports = {
 			filename: 'blocks.[name].build.css',
 			chunkFilename: 'blocks.[name].build.css',
 		} ),
+
+		// Extract @wordpress/scripts plugins.
+		...plugins,
 	],
-	// stats: 'minimal',
+	stats,
 	// Add externals.
 	externals: externals,
 };
